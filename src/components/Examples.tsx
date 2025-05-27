6c6f425a28600b11a4e9cc73f0cf067554902468
miltonlaufer <miltonlaufer@gmail.com>
2025-05-27 03:40:42 +0200

    fix: restore correct microphone reverb routing structure

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -579,7 +579,7 @@ export const useExamples = () => {
           // Use the store's microphone input action which handles permissions
           const micId = await store.addMicrophoneInput({ x: 100, y: 150 })
           const gainId = store.addNode('GainNode', { x: 350, y: 150 })
-          const reverbId = store.addNode('ConvolverNode', { x: 600, y: 150 })
+          const reverbId = store.addNode('ConvolverNode', { x: 600, y: 200 })
           const dryGainId = store.addNode('GainNode', { x: 600, y: 50 })
           const wetGainId = store.addNode('GainNode', { x: 600, y: 300 })
           const mixerId = store.addNode('GainNode', { x: 850, y: 150 })
