310902c39c0eff1e2c13bd7b5e27ffd72b3ab7fa
miltonlaufer <miltonlaufer@gmail.com>
2025-06-02 17:05:23 +0200

    feat: add react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ErrorBoundary from '~/components/ErrorBoundary'
+import { scan } from 'react-scan'
+
+if (!import.meta.env.PROD) {
+  scan({
+    enabled: true,
+  })
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
